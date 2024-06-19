1604688c0c8027e7bac62a2929283d2c1e2a4675
Tbenipal <156356265+Tbenipal@users.noreply.github.com>
2024-06-19 18:58:47 -0400

    Completed website structure
    Completed website structure just left to add some content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("JS Linked");
+
+// Functions
+function showSidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'none';
+}
